5e86613e8fa4dfd194633de5e386ad95446c3914
chirag.kapadia <chirag.kapadia@uber.com>
2023-10-20 12:25:01 -0700

    Add base config for mysql and spire deployment

---
+++ mysql/config/create-mysql-registration.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+set -e
+
+bb=$(tput bold)
+nn=$(tput sgr0)
+
+
+echo "${bb}Creating registration entry for mysql...${nn}"
+kubectl exec -n spire spire-server-0 -- \
+    /opt/spire/bin/spire-server entry create \
+    -spiffeID spiffe://example.org/mysql/server \
+    -parentID spiffe://example.org/ns/spire/sa/spire-agent \
+    -selector k8s:ns:mysql \
+    -selector k8s:pod-label:app:mysql-server
