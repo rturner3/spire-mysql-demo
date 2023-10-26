618f2bf35053bec4ee9c1074e7d1bc4e603fcb0f
chirag.kapadia <chirag.kapadia@uber.com>
2023-10-26 11:03:16 -0700

    Fix image name in script

--- push-mysql-tls-images.sh
+++ push-mysql-tls-images.sh
@@ -3,8 +3,8 @@
 
 # Builds and pushes tls-bootstrap image to https://hub.docker.com/repository/docker/chiragk25/spire-server-tls-bootstrap
 docker build --tag chiragk25/spire-mysql-tls-bootstrap:latest -f Dockerfile.tlsbootstrap .
-docker push chiragk25/spire-server-tls-bootstrap:latest
+docker push chiragk25/spire-mysql-tls-bootstrap:latest
 
 # Builds and pushes tls-reload image to https://hub.docker.com/repository/docker/chiragk25/spire-server-tls-reload
 docker build --tag chiragk25/spire-mysql-tls-reload:latest -f Dockerfile.tlsreload .
-docker push chiragk25/spire-server-tls-reload:latest
+docker push chiragk25/spire-mysql-tls-reload:latest
