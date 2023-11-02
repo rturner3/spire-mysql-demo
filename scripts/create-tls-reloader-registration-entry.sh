9d91ca4a0516600dd177d2ff00252213b16670be
Ryan Turner <turner@uber.com>
2023-11-02 01:48:17 +0000

    Refactor common logic to access spire-server CLI using kubectl

--- scripts/create-tls-reloader-registration-entry.sh
+++ scripts/create-tls-reloader-registration-entry.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source "${SCRIPT_DIR}/common.sh"
+
 bb=$(tput bold)
 nn=$(tput sgr0)
 
 echo "${bb}Creating registration entry for tls-reloader...${nn}"
-kubectl exec -n spire spire-server-0 -- \
-    /opt/spire/bin/spire-server entry create \
+spire_server entry create \
     -spiffeID spiffe://example.org/mysql/client/tls-reloader \
     -parentID spiffe://example.org/ns/spire/sa/spire-agent \
     -x509SVIDTTL 120 \
